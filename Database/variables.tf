2299e4551d51c4fae5c8d6e9fc106500c4949e55
Karim <kernelv5@gmail.com>
2019-08-29 16:59:50 +0800

    Module Update

---
+++ Database/variables.tf
@@ -0,0 +1,21 @@
+
+variable "RGName" {}
+variable "RGLocation" {}
+variable "DBSName" {}
+
+variable "DBSKU_name" {}
+variable "DBSKU_Capacity" {}
+variable "DBSKU_Tier" {}
+variable "DBSKU_Family" {}
+
+
+variable "sp_storage" {}
+variable "sp_backReten" {}
+variable "sp_geoRedunt" {}
+
+variable "admin_user" {}
+variable "admin_login" {}
+variable "dbversion" {}
+variable "dbSSLenf" {}
+variable "DBName" {}
+
